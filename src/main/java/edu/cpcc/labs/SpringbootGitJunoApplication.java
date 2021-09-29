9af355e8eb04c6e0070b3790922f5910cbfb0f3b
cpccharry <harry.subramaniam@cpcc.edu>
2021-09-28 23:14:11 -0400

    Initial release.

---
+++ src/main/java/edu/cpcc/labs/SpringbootGitJunoApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitJunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitJunoApplication.class, args);
+	}
+
+}
